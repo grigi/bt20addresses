5b0fa6867fd7773cb3d01fe38ad11ab7e0e2ed51
Grigi <nagrigoriadis@gmail.com>
2012-06-28 11:44:23 +0200

    All data is now in csv format.

---
+++ data/getdata.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mdb-export -S -X \\\\ Yr13-16\ GPS\ v2.mdb "Yr15 GPS All ID's1(Achive data set) 22May09" > "Yr15 GPS All ID's1(Achive data set) 22May09.csv"
+mdb-export -S -X \\\\ Yr13-16\ GPS\ v2.mdb "Year 13 Mobile Adol Section 1 GPS Coordinates" > "Year 13 Mobile Adol Section 1 GPS Coordinates.csv"
+mdb-export -S -X \\\\ Yr13-16\ GPS\ v2.mdb "Yr16 GPS way points ID fix1" > "Yr16 GPS way points ID fix1.csv"
+mdb-export -S -X \\\\ Yr13-16\ GPS\ v2.mdb "Yr15 GPS Dates updated (5th Jun 09)1 Active" > "Yr15 GPS Dates updated (5th Jun 09)1 Active.csv"
+mdb-export -S -X \\\\ Yr13-16\ GPS\ v2.mdb "Year 16 Mobile Team Appointment DB" > "Year 16 Mobile Team Appointment DB.csv"
+mdb-export -S -X \\\\ Yr13-16\ GPS\ v2.mdb "Yr 16 GPS Updated Dates (active) 9 Jun 091" > "Yr 16 GPS Updated Dates (active) 9 Jun 091.csv"
